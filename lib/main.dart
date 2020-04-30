be792eeb8c5588606021e6c084856e4fbc133026
nonso <nonsoedu14@gmail.com>
2020-04-30 20:19:04 +0100

    third Tax return commit(i am going to fire store it might mess up tho )

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
-import 'package:taxreturn/tax_return_selected.dart';
+import 'package:taxreturn/Screen/home/tax_return.dart';
+import 'package:taxreturn/Screen/home/tax_return_selected.dart';
 import 'package:flutter/material.dart';
-import 'package:taxreturn/tax_return.dart';
+import 'package:taxreturn/Screen/wraper.dart';
+import 'package:taxreturn/module/user.dart';
+import 'package:taxreturn/practice_test.dart';
+import 'package:provider/provider.dart';
+import 'package:taxreturn/services/auth.dart';
 
 
 void main() => runApp(Home());
@@ -9,20 +14,20 @@ void main() => runApp(Home());
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tax Return',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+
+        home: Wrapper(),
+
       ),
-
-      initialRoute: '/',
-      routes: {
-        '/': (context) => TaxReturn(),
-        '/taxReturnSelected': (context) => TaxTypeSelected(),
-      },
-
-
     );
   }
 }
+  //  title: 'Tax Return',
+  //     initialRoute: '/',
+  //     routes: {
+  //       '/': (context) => TaxReturn(),
+  //       '/taxReturnSelected': (context) => TaxTypeSelected(),
+  //       '/practiceTest': (context) => PracticeTest(),
 
